f31ca8cd0f933ae648f35693516d3c7c42f77d2b
Erin Sapp <e.sapp93@gmail.com>
2015-01-21 18:19:06 -0500

    portfolio

---
+++ js/openFile.js
@@ -0,0 +1,15 @@
+<script type="text/javascript">
+
+    function openPdf(e, path, redirect) {
+        // stop the browser from going to the href
+        e = e || window.event; // for IE
+        e.preventDefault();
+
+        // launch a new window with your PDF
+        window.open(path, 'somename', ... /* options */);
+
+        // redirect current page to new location
+        window.location = redirect;
+    }
+
+</script>
